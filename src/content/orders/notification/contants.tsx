a42fae06cca85d4b9494ecd99f1ee683c83fd080
skydev <skydev55@outlook.com>
2022-04-20 06:02:15 -0700

    full or split screen for notifications

---
+++ src/content/orders/notification/contants.tsx
@@ -0,0 +1,12 @@
+export const NOTIFICATIONS = [
+  {
+    title: 'New Order',
+    count: 1,
+    class: 'success'
+  },
+  {
+    title: 'Delayed Orders',
+    count: 2,
+    class: 'warning'
+  }
+];
